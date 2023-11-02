1e98f32c4dab7e3053254c3c8f2bc1cc37bcba7b
kookjin <aingdevil10@naver.com>
2023-11-02 15:25:37 +0900

    jpa 기본키 생성 및 컬럼 맵핑

--- ex-hello-jpa/ex1-hello-jpa/src/main/java/helloJpa/JpaMain.java
+++ ex-hello-jpa/ex1-hello-jpa/src/main/java/helloJpa/JpaMain.java
@@ -17,13 +17,13 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            ArrayList<Member> members = new ArrayList<>();
+//            ArrayList<Member> members = new ArrayList<>();
             // 로직
-            for(Long i = 1l; i < 30; i++){
-                Member member = new Member(i, "newMember" + i);
-                members.add(member);
-                em.persist(member);
-            }
+//            for(Long i = 1l; i < 30; i++){
+//                Member member = new Member(i, "newMember" + i);
+//                members.add(member);
+//                em.persist(member);
+//            }
             // 비영속
 //            Member member1 = new Member(101L, "jin1");
 //            Member member2 = new Member(102L, "jin2");
@@ -32,10 +32,10 @@ public class JpaMain {
 //            em.persist(member1); //단일 컨텍스트 저장
 //            em.persist(member2);
 
-            System.out.println("=========");
-
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember = " + findMember.getName());
+//            System.out.println("=========");
+//
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getName());
 //            Member member = em.find(Member.class, 1L);
 //            List<Member> memberList = em.createQuery("select m from Member as m", Member.class)
 //                    .setFirstResult(0) // 1번부터
--- ex-hello-jpa/ex1-hello-jpa/src/main/java/helloJpa/Member.java
+++ ex-hello-jpa/ex1-hello-jpa/src/main/java/helloJpa/Member.java
@@ -1,34 +1,97 @@
 package helloJpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// db 방언에 맞게 알아서 지정
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name", updatable = false/*jpa update조작 불가 */, insertable = false/*jpa insert조작 불가 */)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
     public Member(){
 
     }
-    public Member(Long id, String name) {
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
+
