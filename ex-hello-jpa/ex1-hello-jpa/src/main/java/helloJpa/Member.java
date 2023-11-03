2bc531a08055664d5d49c9cec60c7b281e06a588
kookjin <aingdevil10@naver.com>
2023-11-03 10:58:47 +0900

    JPA 상품구매 실습

--- ex-hello-jpa/ex1-hello-jpa/src/main/java/helloJpa/Member.java
+++ ex-hello-jpa/ex1-hello-jpa/src/main/java/helloJpa/Member.java
@@ -8,7 +8,13 @@ import java.util.Date;
 @SequenceGenerator(
         name = "MEMBER_SEQ_GENERATOR", // spring 에서 매칭시킬 시퀀스 네임
         sequenceName = "MEMBER_SEQ", // 매핑할 DB상의 시퀀스 네임
-        initialValue = 1, allocationSize = 50) // 50개 단위로 시퀀스값을 가져온다 예)현제5352 = 5353~5402까지
+        initialValue = 1, allocationSize = 50)
+// allocationSize 는 여러 서버가 작동하더라도 각 서버가 할당받은 시퀀스를 확보하기때문에
+// 동시성 문제도 없어짐
+// 50개 단위로 시퀀스값을 가져온다
+// 예)현재5352 = 5353~5402까지
+// TABLE 도 같은 전략으로 가져오기때문에 문제없음
+// hibernate5 버전 이후부터는 default 값이 TABLE
 public class Member {
     @Id
     @GeneratedValue(
