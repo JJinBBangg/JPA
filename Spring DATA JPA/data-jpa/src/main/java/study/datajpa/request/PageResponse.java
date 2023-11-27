0442cc4ebbf06c1359a4e7d00e2e0892a3e5375b
kookjin <aingdevil10@naver.com>
2023-11-27 09:18:48 +0900

    jpa 컨버터 활용 pageable

--- Spring DATA JPA/data-jpa/src/main/java/study/datajpa/request/PageResponse.java
+++ Spring DATA JPA/data-jpa/src/main/java/study/datajpa/request/PageResponse.java
@@ -4,6 +4,9 @@ package study.datajpa.request;
 
 public class PageResponse {
     // 아래 참고해서 paging 관련 정보 dto 생성
+    // 위 처럼 생성해도되고
+    // Page<Member> Page<MemberResponse> 변환가능
+    // 아래 page관련 정보와 List타입의 MemberResponse 롤 반환됨
     /*"pageable": {
 "pageNumber": 0,
 "pageSize": 30,
